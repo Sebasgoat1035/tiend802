6702d5ce7b43f7fc06d87f72600274514809914f
Sebasgoat1035 <contrerassebas90@gmail.com>
2026-07-01 07:19:39 -0500

    Corregir URL HTTPS

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://proyectapi802.somee.com';
+const API_URL = "https://proyectapi802.somee.com";
 
 const api = axios.create({
     baseURL: API_URL,
